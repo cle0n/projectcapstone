036059d653b081b195b6093b9dcd57fc434a3595
Swerzi <denniel@sheridanc.on.ca>
2018-03-15 06:38:46 -0700

    Create nestedloop.c
    Source code

---
+++ Code Samples/nestedloop.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main (int argc, char *argv[]){
+
+/* local variable definition */
+int x, y,z ;
+for(x = 0; x < 2; x++) {
+
+	for(y = 0; y < 2; y++){
+
+		for (z = 0; z < 2; z++){
+			printf("Welcome to the nest.\n");
+	  }
+ }
+ 
+}
+
+return 0;
+}
