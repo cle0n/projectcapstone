6da6b7a1b155f18990f2af41e0806b6bbe3033ae
cle0n <s.condotta@gmail.com>
2018-03-15 21:04:10 -0400

    Added while loop example + updated Makefile

---
+++ tests/asmsrc/WLOOP0.asm
@@ -0,0 +1,57 @@
+;----------------------------------------
+;
+;	while loop construct #1 (64-bit)
+;	with RIP relative addressing
+;
+;	build:
+;	nasm -felf64 WLOOP.asm
+;	gcc -nostartfiles WLOOP.o -o WLOOP.out
+;
+;----------------------------------------
+
+		extern	puts
+		extern	exit
+
+		global	_start
+		
+		SECTION	.data
+msg:	db	'Hello World!', 0
+
+;----------------------------------------
+;
+;	GLOBAL:
+;		char	*msg = "Hello World!";
+;	LOCAL:
+;		long	ret = 4;
+;
+;	while(ret-- > 0)
+;		puts("Hello World!");
+;
+;----------------------------------------
+
+%macro	ccall 1
+		call	%1 WRT ..plt
+%endmacro
+
+
+		SECTION	.text
+_start:
+		push	rbp
+		mov		rbp, rsp
+		sub		rsp, 8
+		mov		qword [rbp - 8], 4
+		jmp		L0
+L1:
+		lea		rdi, [rel + msg]
+		ccall	puts
+L0:
+		mov		rax, qword [rbp - 8]
+		lea		rdx, [rax - 1]
+		mov		qword [rbp - 8], rdx
+		test	rax, rax
+		jg		L1
+LX:
+		ccall	exit
+
+; SHIFT + K over name of C function
+; looks up its manpage and opens it what.
