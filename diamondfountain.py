521d731e6813df6f7f9e7fc4f353cefd9b15919b
cle0n <s.condotta@gmail.com>
2018-05-23 00:32:18 -0400

    modified to use in r2 session

--- diamondfountain.py
+++ diamondfountain.py
@@ -4,11 +4,29 @@
 
 	https://rkx1209.github.io/2017/08/27/gsoc-final-report.html
 	
-	- hook API's and modify return value
 	- emulate fs segment register for runtime API loading
 	- look into de? dt? ds? dts?
 	  > dts may be useful for state tracking
+	  > (just see how things are done and replicate them in python)
 
+	- command is read first before anything. obviously run init first then if
+	
+	- upon script initialization set a variable or something in r2 session so
+	  that when invoking this script again only the pipe has to be opened
+	
+	- script should look at all imports and save them in a lookup table for
+	  call hook to look at
+	  
+	- for malloc calls use aeim [address] [space] to allocate space. Save EBP 
+	  and ESP beforehand then restore them right after.
+	  
+	- emulate TEB + PEB. initialize memory with aeim like above. Populate it with
+	  mock values. Need to figure out how to load modules
+	
+	
+	run this plugin in a r2 session like this:
+	[0x00401000]> . /[path to plugin].py [args]
+		
 '''
 
 import os
@@ -24,18 +42,26 @@ class avm_strings:
 		'RegOpenKey',
 	]
 
+#   INITIALIZATION STUFF
+###################################################################################
+def InitPEB():
+	return
+
+'''
+	Look at all symbols with 'isj' or something. Get there values and put them
+	in a dictionary
+'''
+def InitSymbols():
+	return
+
 def InitEmu(r2, BITS=32, ARCH='x86'):
 	r2.cmd('aaaa')
-	# Enable modification of code in memory and on disk
 	r2.cmd('e io.cache=true')
 	r2.cmd('e asm.bits=' + str(BITS))
 	r2.cmd('e asm.arch=' + ARCH)
 	r2.cmd('e asm.emu=true')
-	# Initialize ESIL VM
 	r2.cmd('aei')
-	# Set the starting point
 	r2.cmd('aeip')
-	# Initialize 0x2000 bytes of stack space at 0xFFFFD000
 	r2.cmd('aeim 0xFFFFD000 0x2000 stack')
 
 def Init():
@@ -44,47 +70,85 @@ def Init():
 	args   = parser.parse_args()
 	
 	if not args.arg:
-		print "USAGE: diamondfountain.py [infile]"
-		exit(1)
-		
-	r2 = r2pipe.open(os.path.realpath(args.arg[0]))
+		r2 = r2pipe.open()
+	else:
+		r2 = r2pipe.open(os.path.realpath(args.arg[0]))
 	
 	return r2
+
+#   INSTRUCTION HOOKS
+###################################################################################	
+def hook_call(r2, c):
+	print "! Got CALL"
+	
+	# if call goes to API then need to lookup API in symbol dict and call its 
+	# handler and-or modify the return value
+	
+	# call someimmediateoffset
+	if 'jump' in c[0]:
+		print "  > CALL IMM"
+		return 0
+	else:
+		# call eax
+		# Check whats in eax and see if it is in the symbol dictionary
+		if 'reg' == c[0]['opex']['operands'][0]['type']:
+			print "  > CALL REG"
+			return 1
+	return 1
+
+# Hook Table
+hooks = {
+	'call' : hook_call,
+}
+
+
+#   COMMANDS
+###################################################################################
+def e_cont(r2):
+	while True:
+		r2.cmd('aes')
+		insn = r2.cmdj('aoj @ eip')
+		
+		if insn[0]['mnemonic'] in hooks:
+			ret = hooks[insn[0]['mnemonic']](r2, insn)
+			if ret == 1:
+				r2.cmd('s eip') # set currseek
+				break
+	return
+	
+'''
+[0x401000]> . [plugin].py --command [cmd]
+
+'''
+def evalcommand():
+	return
 
 def main():
 	r2 = Init()
 	InitEmu(r2)
 	
+	# test call
+	e_cont(r2)
+	
+	# evalcommand()
+	
+	# Put me in Init section
+	'''
 	symbols = r2.cmdj('isj')
 	
-	# What if there is more than one reference to RegOpenKey, or multiple kinds
-	# of RegOpenKey functions?
-	for sym in symbols:
-		if avm_strings.susp_api[0] in sym['name']:
-			api_regopenkey = sym['name']
+	# axt - find references to some api
+	usageloc = r2.cmdj('axtj @ sym.' + someapi)
+	'''
 	
-	# Check to see if api_regopenkey was assigned a value
-	try:
-		api_regopenkey
-	except NameError:
-		print "No references to RegOpenKey detected"
-		r2.quit()
-		exit(0)
-	else:
-		print "Reference to RegOpenKey detected"
 	
-	# What if RegOpenKey is used more than once?
-	# axt - find references of regopenkey
-	usageloc = r2.cmdj('axtj @ sym.' + api_regopenkey)
-	
-	# emulate code until address
-	r2.cmd('aesu ' + hex(usageloc[0]['from']))
-	
+	#Put me in RegOpenKey emulation function
+	'''
 	strarg = r2.cmd('psw @ [esp+4]')
 	
 	if avm_strings.susp_reg_key[0] == strarg:
 		print "! @ " + hex(usageloc[0]['from']) + " VMware registry key check"
 		print "  > ARG: '" + strarg + "'"
+	'''
 	
 	r2.quit()
 	
