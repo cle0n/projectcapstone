6f11151b18d1239c5dc981c939420ec044f29868
cle0n <s.condotta@gmail.com>
2018-05-29 23:50:06 -0400

    updated. check out TODO

--- diamondfountain.py
+++ diamondfountain.py
@@ -14,20 +14,34 @@
 	*Visual Mode not supported.
 	*Certain r2 commands won't work
 	
+	QUESTIONS:
+	- How to tell if stdcall or cdecl?
 	
 	TODO:
-	- For API calls, need to determine how many arguments were pushed on the
-	  stack so we can adjust it accordingly after emulation. (assuming stdcall)
-	- How to determine if cdecl or stdcall? Let user decide? what if obfuscated?
-	- What if CALL -> JMP like wprintf?? ( Monitor EIP instead of CALL insn )
 	- Do better instruction parsing. Not every command needs r2 or eapi.
+	
 	- Have some command to view internal data structure that we manage and be
 	  able to edit them?
+	
 	- Build a configuration for each API (stack arguments, return values, etc)
+	
+	- add hooks for memory read/write/execute 
+	
+	- test out revolver-style task calling
+	
+	- PEB/TEB at offset 0x30 fs:0x30 fs is fucked so esil just accesses 0x30
+	  what about the other segments like gs? on 64-bit
+	  o load kernel32 and ntdll 32 and create a PEB
+	 
+	- SEH, fs:[0]. How the hell to detect exceptions?
+	
+	- Kill r2 session. Start r2 session in script.
 
 '''
 
+import readline
 import r2pipe
+#import pefile
 
 
 class ApiEmu:
@@ -37,46 +51,123 @@ class ApiEmu:
 	
 	def __init__(self):
 		self.SYMBOLS = {
-			'RegOpenKeyExW': self._RegOpenKeyExW,
-			'RegCloseKey'  : self._RegCloseKey,
-			'ExitProcess'  : self._ExitProcess,
-			'_wprintf'     : self.__wprintf,
+			'diamond_def'              : self._DiamondDefault,
+			'RegOpenKeyExW'            : self._RegOpenKeyExW,
+			'RegCloseKey'              : self._RegCloseKey,
+			'ExitProcess'              : self._ExitProcess,
+			'GetAdaptersAddresses'     : self._GetAdaptersAddresses,
+			'GetProcessHeap'           : self._GetProcessHeap,
+			'HeapAlloc'                : self._HeapAlloc,
+			'HeapFree'                 : self._HeapFree,
+			'StrCmpNI'                 : self._StrCmpNI,
+			'StrRStrIW'                : self._StrRStrIW,
+			'ExpandEnvironmentStringsW': self._ExpandEnvironmentStrings,
+			'FindFirstFileW'           : self._FindFirstFileW,
+			'FindClose'                : self._FindClose,
+			'EnumProcesses'            : self._EnumProcesses,
+			'OpenProcess'              : self._OpenProcess,
+			'CloseHandle'              : self._CloseHandle,
+			'GetModuleFileNameExW'     : self._GetModuleFileNameExW,
+			'OpenSCManagerW'           : self._OpenSCManagerW,
+			'EnumServiceStatusW'       : self._EnumServiceStatusW,
+			'CloseServiceHandle'       : self._CloseServiceHandle,
+			'GetLastError'             : self._GetLastError,
+			'wprintf'                  : self._wprintf,
+			'_snwprintf'               : self.__snwprintf,
+			'memset'                   : self._memset,
+			'wcslen'                   : self._wcslen,
 		}
 		
+	def _DiamondDefault(self, r2):
+		print "! Unknown API"
+		print r2.cmd('pd 1 @ eip')
+		return
+
+	def _GetAdaptersAddresses(self, r2):
+		print "! GetAdaptersAddresses #TODO"
+	def _GetProcessHeap(self, r2):
+		print "! GetProcessHeap #TODO"
+	def _HeapAlloc(self, r2):
+		print "! HeapAlloc #TODO"
+	def _HeapFree(self, r2):
+		print "! HeapFree #TODO"
+	def _StrCmpNI(self, r2):
+		print "! StrCmpNI #TODO"
+	def _StrRStrIW(self, r2):
+		print "! StrRStrIW #TODO"
+	def _ExpandEnvironmentStrings(self, r2):
+		print "! ExpandEnvironmentStrings #TODO"
+	def _FindFirstFileW(self, r2):
+		print "! FindFirstFileW #TODO"
+	def _FindClose(self, r2):
+		print "! FindClose #TODO"
+	def _EnumProcesses(self, r2):
+		print "! EnumProcesses #TODO"
+	def _OpenProcess(self, r2):
+		print "! OpenProcess #TODO"
+	def _CloseHandle(self, r2):
+		print "! CloseHandle #TODO"
+	def _GetModuleFileNameExW(self, r2):
+		print "! GetModuleFileNameExW #TODO"
+	def _OpenSCManagerW(self, r2):
+		print "! OpenSCManagerW #TODO"
+	def _EnumServiceStatusW(self, r2):
+		print "! EnumServiceStatusW #TODO"
+	def _CloseServiceHandle(self, r2):
+		print "! CloseServiceHandle #TODO"
+	def _GetLastError(self, r2):
+		print "! GetLastError #TODO"
+	def __snwprintf(self, r2):
+		print "! _snwprintf #TODO"
+	def _memset(self, r2):
+		print "! memset #TODO"
+	def _wcslen(self, r2):
+		print "! wcslen #TODO"
+		
 	def _RegOpenKeyExW(self, r2):
-		print "RegOpenKeyExW"
-		r2.cmd('.ar-; ar eip=[esp]; ar esp=esp+4')
+		print "! RegOpenKeyExW"
 		
 		strarg = r2.cmd('psw @ [esp+4]')
 		
 		if strarg in ApiEmu.susp_reg_key:
 			print "! VMware registry key check"
-			print "  > ARG: '" + strarg + "'"
+		
+		print "  > ARG: '" + strarg + "'"
 		
 		r2.cmd('ar eax=0')
 		
 		return
 	
 	def _RegCloseKey(self, r2):
-		print "RegCloseKey"
-		r2.cmd('.ar-; ar eip=[esp]; ar esp=esp+4')
+		print "! RegCloseKey"
 		r2.cmd('ar eax=0')
 		return
 		
 	def _ExitProcess(self, r2):
-		print "ExitProcess"
-		r2.cmd('.ar-; ar eip=[esp]; ar esp=esp+4')
+		print "! ExitProcess"
 		return
 		
-	def __wprintf(self, r2):
-		print "_wprintf"
-		r2.cmd('.ar-; ar eip=[esp]; ar esp=esp+4')
+	def _wprintf(self, r2):
+		print "! _wprintf #TODO"
 		r2.cmd('ar eax=0')
 		return
 
 #   COMMAND FUNCTIONS
 ###################################################################################
-def Build_TEB_PEB(r2, eapi):
+def Build_TEB_PEB(r2, eapi=None):
+	
+	EBP = r2.cmd('ar ebp')
+	ESP = r2.cmd('ar esp')
+	
+	r2.cmd('aeim 0x0 0x40')      # for fs:[0x30] or fs:[0]
+	r2.cmd('aeim 0x1000 0x1000') # for PEB
+	
+	# use wv to write addresses
+	r2.cmd('wv 0x1000 @ 0x30')
+	
+	# TODO: finish inner PEB structures
+	# TODO: map kernel32 + ntdll + kernelbase.
+	
 	return
 
 SYMBOLS = { }
@@ -84,9 +175,15 @@ SYMBOLS = { }
 def BuildSymbols(r2, eapi):
 	for symbol in r2.cmdj('isj'):
 		content = r2.cmdj('pxrj 4 @ ' + hex(symbol['vaddr']))
-		for API in eapi.SYMBOLS:
+		for i, API in enumerate(eapi.SYMBOLS):
 			if API in symbol['flagname']:
+				print "Got: " + API
 				SYMBOLS[content[0]['value']] = eapi.SYMBOLS[API]
+				i = 0
+				break
+		if i == len(eapi.SYMBOLS) - 1:
+			print "Unknown API: " + symbol['flagname']
+			SYMBOLS[content[0]['value']] = eapi.SYMBOLS['diamond_def']
 	
 	#usageloc = r2.cmdj('axtj @ sym.' + someapi)
 
@@ -103,9 +200,14 @@ def InitEmu(r2, eapi=None, BITS=32, ARCH='x86'):
 def Continue(r2, eapi=None):
 	while True:
 		r2.cmd('aes')
+		addr = int(r2.cmd('s'), 0)
+		
+		if addr in SYMBOLS:
+			r2.cmd('.ar-; ar eip=[esp]; ar esp=esp+4')
+			SYMBOLS[addr](r2)
+			break
+		
 		insn = r2.cmdj('aoj @ eip')
-
-		# check if EIP is in API hooks list instead--or in addtion to CALL?
 		
 		if insn[0]['mnemonic'] in IHOOKS:
 			ret = IHOOKS[insn[0]['mnemonic']](r2, insn)
@@ -134,16 +236,6 @@ COMMANDS = {
 def HOOK_CALL(r2, insn):
 	if 'jump' in insn[0]:
 		print "! CALL " + hex(insn[0]['jump'])
-	else:
-		# step into the CALL
-		r2.cmd('aes')
-		addr = int(r2.cmd('s'), 16)
-		if addr in SYMBOLS:
-			SYMBOLS[addr](r2)
-		else:
-			r2.cmd('.ar-; ar eip=[esp]; ar esp=esp+4')
-			# adjust sp and ip to recover from effects of the CALL
-			# does not account for arguments on stack! 
 		
 	return 1
 
@@ -164,6 +256,7 @@ def main():
 		command = raw_input('[' + r2.cmd('s') + ']> ')
 		
 		if command == "stop":
+			r2.cmd('-ar* ; ar0 ; aeim- ; aei-;')
 			r2.quit() # doesn't always quit for some reason. Use pkill r2.
 			break
 		
