1615a34ea00479c51076514bca3d1fc14c189f2e
ian <lobotomization@gmail.com>
2018-06-06 14:28:11 -0700

    Added slightly better command parsing. Added more hooks. Added a bunch of garbage under HOOK_CALL because not every call was being hooked, this definitely needs fixing

--- diamondfountain.py
+++ diamondfountain.py
@@ -43,6 +43,8 @@
 
 import readline
 import r2pipe
+import random
+import sys
 #import pefile
 
 
@@ -71,6 +73,7 @@ class ApiEmu:
 			'OpenProcess'              : self._OpenProcess,
 			'CloseHandle'              : self._CloseHandle,
 			'GetModuleFileNameExW'     : self._GetModuleFileNameExW,
+			'GetModuleFileNameA'	   : self._GetModuleFileNameA,
 			'OpenSCManagerW'           : self._OpenSCManagerW,
 			'EnumServiceStatusW'       : self._EnumServiceStatusW,
 			'CloseServiceHandle'       : self._CloseServiceHandle,
@@ -79,6 +82,17 @@ class ApiEmu:
 			'_snwprintf'               : self.__snwprintf,
 			'memset'                   : self._memset,
 			'wcslen'                   : self._wcslen,
+           		'GetSystemTimeAsFileTime'  : self._GetSystemTimeAsFileTime,
+            		'GetCurrentProcessId'      : self._GetCurrentProcessId,
+		        'GetCurrentThreadId'       : self._GetCurrentThreadId,
+        		'GetTickCount'             : self._GetTickCount,
+        		'QueryPerformanceCounter'  : self._QueryPerformanceCounter,
+			'LoadLibraryA'		   : self._LoadLibraryA,
+			'GetProcAddress'	   : self._GetProcAddress,
+			'SetErrorMode'		   : self._SetErrorMode,
+			'GetCurrentDirectoryA'	   : self._GetCurrentDirectoryA,
+			'TlsGetValue'		   : self._TlsGetValue,
+			'GlobalUnfix'		   : self._GlobalUnfix,
 		}
 		
 	def _DiamondDefault(self, r2):
@@ -91,30 +105,21 @@ class ApiEmu:
 	def _GetProcessHeap(self, r2):
 		print "! GetProcessHeap #TODO"
 	def _HeapAlloc(self, r2):
-		print "! HeapAlloc #TODO"
+		print "! HeapAlloc"
+		r2.cmd("ar esp=esp+12")
+		r2.cmd("ar eax=1")
+		return
+
 	def _HeapFree(self, r2):
 		print "! HeapFree #TODO"
 	def _StrCmpNI(self, r2):
 		print "! StrCmpNI #TODO"
-	def _StrRStrIW(self, r2):
-		print "! StrRStrIW #TODO"
 	def _ExpandEnvironmentStrings(self, r2):
 		print "! ExpandEnvironmentStrings #TODO"
 	def _FindFirstFileW(self, r2):
 		print "! FindFirstFileW #TODO"
 	def _FindClose(self, r2):
 		print "! FindClose #TODO"
-	def _EnumProcesses(self, r2):
-		print "! EnumProcesses #TODO"
-		r2.cmd('wx 562 @ [esp]')
-		r2.cmd('wx 4 @ [esp+8]')
-		r2.cmd('ar eax=256')
-	def _OpenProcess(self, r2):
-		print "! OpenProcess #TODO"
-	def _CloseHandle(self, r2):
-		print "! CloseHandle #TODO"
-	def _GetModuleFileNameExW(self, r2):
-		print "! GetModuleFileNameExW #TODO"
 	def _OpenSCManagerW(self, r2):
 		print "! OpenSCManagerW #TODO"
 	def _EnumServiceStatusW(self, r2):
@@ -157,6 +162,96 @@ class ApiEmu:
 		print "! _wprintf #TODO"
 		r2.cmd('ar eax=0')
 		return
+	def _GetSystemTimeAsFileTime(self, r2):
+		print "! GetSystemTimeAsFileTime"
+		r2.cmd('ar esp=esp+4')
+
+	def _GetCurrentProcessId(self, r2):
+		print "! GetCurrentProcessId"
+		r2.cmd('ar eax=1')
+
+	def _GetCurrentThreadId(self, r2):
+		print "! GetCurrentThreadId"
+		r2.cmd('ar eax=1')
+
+	def _GetTickCount(self, r2):
+		print "! GetTickCount"
+		r2.cmd('ar eax=1')
+
+	def _QueryPerformanceCounter(self, r2):
+		print "! QueryPerformanceCounter"
+		r2.cmd('ar esp=esp+4')
+		r2.cmd('ar eax=1')
+		
+	def _OpenProcess(self, r2):
+		print "! OpenProcess"
+		r2.cmd('ar esp=esp+12')
+		r2.cmd('ar eax=1111')
+
+	def _CloseHandle(self, r2):
+		print "! CloseHandle"
+		r2.cmd('ar esp=esp+4')
+		r2.cmd('ar eax=1')
+
+	def _GetModuleFileNameExW(self, r2):
+		print "! GetModuleFileNameExW"
+		r2.cmd('ar esp=esp+12')
+		r2.cmd('ar eax=1')
+
+	def _StrRStrIW(self, r2):
+		print "! StrRStrIW"
+		print "  > ARG: " + r2.cmd('psw @ [esp+8]')
+		r2.cmd('ar esp=esp+12')
+		r2.cmd('ar eax=1')
+
+	def _EnumProcesses(self, r2):
+		print "! EnumProcesses"
+		r2.cmd('wv 0x666 @ [esp]')
+		r2.cmd('wv 0x4 @ [esp+8]')
+		r2.cmd('ar esp=esp+12')
+		r2.cmd('ar eax=1')
+
+	def _LoadLibraryA(self, r2):
+		print "! LoadLibrary"
+		r2.cmd("ar esp=esp+4")
+		r2.cmd("ar eax=1")
+		return
+
+	def _GetProcAddress(self, r2):
+		print "! GetProcAddress"
+		r2.cmd("ar esp=esp+8")
+		r2.cmd("ar eax=1")
+		return
+
+	def _SetErrorMode(self, r2):
+		print "! SetErrorMode"
+		r2.cmd("ar esp=esp+4")
+		r2.cmd("ar eax=1")
+		return
+
+	def _GetModuleFileNameA(self, r2):
+		print "! GetModuleFileNameA"
+		r2.cmd("ar esp=esp+12")
+		r2.cmd("ar eax=1")
+		return
+
+	def _GetCurrentDirectoryA(self, r2):
+		print "! GetCurrentDirectoryA"
+		r2.cmd("ar esp=esp+8")
+		r2.cmd("ar eax=1")
+		return
+
+	def _TlsGetValue(self, r2):
+		print "! TlsGetValue"
+		r2.cmd('ar esp=esp+4')
+		r2.cmd('ar eax=1')
+		return
+
+	def _GlobalUnfix(self, r2):
+		print "! GlobalUnfix"
+		r2.cmd('ar esp=esp+4')
+		r2.cmd('ar eax=1')
+		return
 
 #   COMMAND FUNCTIONS
 ###################################################################################
@@ -178,7 +273,7 @@ def Build_TEB_PEB(r2, eapi=None):
 
 SYMBOLS = { }
 
-def BuildSymbols(r2, eapi):
+def BuildSymbols(r2, eapi, dummy0=None):
 	for symbol in r2.cmdj('isj'):
 		content = r2.cmdj('pxrj 4 @ ' + hex(symbol['vaddr']))
 		for i, API in enumerate(eapi.SYMBOLS):
@@ -193,8 +288,9 @@ def BuildSymbols(r2, eapi):
 	
 	#usageloc = r2.cmdj('axtj @ sym.' + someapi)
 
-def InitEmu(r2, eapi=None, BITS=32, ARCH='x86'):
+def InitEmu(r2, eapi=None, dummy0=None, BITS=32, ARCH='x86'):
 	# Actual init stuff
+	r2.cmd('e anal.bb.maxsize=16384')
 	r2.cmd('aaaa')
 	r2.cmd('e io.cache=true')
 	r2.cmd('e asm.bits=' + str(BITS))
@@ -221,30 +317,45 @@ def InitEmu(r2, eapi=None, BITS=32, ARCH='x86'):
 		IHOOKS[insn[0]['mnemonic']](r2, insn, eapi)
 
 
-def Continue(r2, eapi=None):
-	while True:
-		# Found a bit of a subtle bug here.
-		# If the very first instruction should be hooked
-		# it would've been skipped by the aes command
-		# before we could analyze it.
-		r2.cmd('aes')
-		addr = int(r2.cmd('s'), 0)
+def Continue(r2, eapi=None, args=None):
+	count = 1
+	if args:
+		try:
+			count = int(args[0])
+		except ValueError:
+			print "Invalid argument"
+			return
+	while count > 0:	
+		while True:
+			# Found a bit of a subtle bug here.
+			# If the very first instruction should be hooked
+			# it would've been skipped by the aes command
+			# before we could analyze it.
+			r2.cmd('aes')
+			addr = int(r2.cmd('s'), 0)
 		
-		if addr in SYMBOLS:
-			r2.cmd('.ar-; ar eip=[esp]; ar esp=esp+4')
-			SYMBOLS[addr](r2)
-			break
-		
-		insn = r2.cmdj('aoj @ eip')
-		
-		if insn[0]['mnemonic'] in IHOOKS:
-			ret = IHOOKS[insn[0]['mnemonic']](r2, insn, eapi)
-			if ret == 1:
+			if addr in SYMBOLS:
+				r2.cmd('.ar-; ar eip=[esp]; ar esp=esp+4')
+				SYMBOLS[addr](r2)
 				break
+		
+			insn = r2.cmdj('aoj @ eip')
+		
+			if insn[0]['mnemonic'] in IHOOKS:
+				ret = IHOOKS[insn[0]['mnemonic']](r2, insn, eapi)
+				if ret == 1:
+					break
 
-	r2.cmd('s eip')
+			for op in insn[0]['opex']['operands']:
+			    if 'disp' and 'segment' in op and op['segment'] == 'fs':
+				print "! FS:0 detected. Adjust manually before continuing."
+				r2.cmd('s eip')
+				break # Used to be return
+		count -= 1
+		r2.cmd('s eip')
 
-def Help(dummy0=None, dummy1=None):
+
+def Help(dummy0=None, dummy1=None, dummy2=None):
 	HELP = """COMMANDS:
 	init - Initializes ESIL VM
 	symb - Builds list of imports links known ones to our emulated API's
@@ -264,12 +375,44 @@ COMMANDS = {
 ###################################################################################
 def HOOK_CALL(r2, insn, eapi):
 	if 'jump' in insn[0]:
+		r2.cmd('s eip') # Might need this for an annoying bug...
 		print "! CALL " + hex(insn[0]['jump'])
 		
 		# When a call is made add the expected return value to the stack
-		expected_ret_val = hex(r2.cmdj('aoj 2')[1]['addr'])
+		expected_ret_val = hex(r2.cmdj('aoj 2')[-1]['addr'])
 		print "! EXPECTED RETURN " + expected_ret_val
+		if not r2.cmdj('pdfj @ ' + hex(insn[0]['jump'])):
+			print "! INVALID FUNCTION CALL"
 		eapi.ret_stk.append(expected_ret_val)
+
+	# Turns out there are other kinds of calls that aren't being properly checked
+	# Stuff like call dword [0x41f004] for instance
+	if 'ptr' in insn[0]:
+		if int(insn[0]['ptr']) in SYMBOLS:
+			return 1
+
+		r2.cmd('s eip') # Might need this for an annoying bug...
+		print "! CALL " + hex(insn[0]['ptr'])
+		
+		# When a call is made add the expected return value to the stack
+		expected_ret_val = hex(r2.cmdj('aoj 2')[-1]['addr'])
+		print "! EXPECTED RETURN " + expected_ret_val
+		if not r2.cmdj('pdfj @ ' + hex(insn[0]['ptr'])):
+			print "! INVALID FUNCTION CALL"
+		else:
+			eapi.ret_stk.append(expected_ret_val)
+
+	if 'reg' in insn[0]:
+		r2.cmd('s eip') # Might need this for an annoying bug...
+		print "! CALL " + r2.cmd('ar ' + insn[0]['reg'])
+		
+		# When a call is made add the expected return value to the stack
+		expected_ret_val = hex(r2.cmdj('aoj 2')[-1]['addr'])
+		print "! EXPECTED RETURN " + expected_ret_val
+		if not r2.cmdj('pdfj @ ' + r2.cmd('ar ' + insn[0]['reg'])):
+			print "! INVALID FUNCTION CALL"
+		else:
+			eapi.ret_stk.append(expected_ret_val)
 	
 		
 	return 1
@@ -300,23 +443,31 @@ IHOOKS = {
 def main():
 	r2   = r2pipe.open('http://127.0.0.1:9090')
 	eapi = ApiEmu()
-	
-	print r2.cmd('?E Bruh')
+	bro = random.choice(['Bruh', 'Bro', 'Breh', 'Brah', 'Broseph', 'Brocahontas', 'Brometheous'])
+	print r2.cmd('?E ' + bro)
 	print "Enter help for a list of commands."
 
 	while True:
-		command = raw_input('[' + r2.cmd('s') + ']> ')
-		
+		try:
+			commandAndArgs    = raw_input('[' + r2.cmd('s') + ']> ')
+		except EOFError:
+			print '[' + r2.cmd('s') + ']> '
+			commandAndArgs    = sys.stdin.readline()
+		commandAndArgsArr = commandAndArgs.split()
+		command           = commandAndArgsArr[0]
+		args = []		
+		if len(commandAndArgsArr) > 1:
+			args      = commandAndArgsArr[1:]
+
 		if command == "stop":
 			r2.cmd('-ar* ; ar0 ; aeim- ; aei-;')
 			r2.quit() # doesn't always quit for some reason. Use pkill r2.
 			break
 		
 		if command in COMMANDS:
-			COMMANDS[command](r2, eapi) # do better instruction parsing
+			COMMANDS[command](r2, eapi, args) # do better instruction parsing
 		else:
-			print r2.cmd(command)
-
+			print r2.cmd(commandAndArgs)
 
 
 	
