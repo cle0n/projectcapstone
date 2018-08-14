8949f04020071e5177360276c27f3efbf2d5724e
Swerzi <denniel@sheridanc.on.ca>
2018-08-14 11:59:10 -0400

    Add files via upload

--- tests/csrc/breakpoint.c
+++ tests/csrc/breakpoint.c
@@ -1,15 +1,27 @@
-#include <signal.h>
+/*
+ * Make 0xCC instruction length larger then 1
+ */
+#include <stdio.h>
+#include <sys/mman.h>
+#include <stdlib.h>
 
+typedef void(*FUN)(void);
+char * myFunc;
 
-int main(int argc, char** argv)
+char *allocExecutablePages (int pages)
 {
-    raise(SIGINT);
-    int a = 3;
-    __asm__("int3");
-    a++;  
-    raise(SIGINT);
-	printf("vboxoglfeedbackspu.dll")
-    return 0;
+    char *t = valloc (getpagesize() * pages);
+    if (mprotect (t, getpagesize(),  PROT_READ|PROT_EXEC|PROT_WRITE) == -1) {
+        fprintf (stderr, "mprotect");
+    }
+    return t;
+}
 
-
+void main(void) {
+    myFunc = allocExecutablePages(1);
+    myFunc[0] = 0x67; // add redundant prefix
+    myFunc[1] = 0x67; // add redundant prefix
+    myFunc[2] = 0x67; // add redundant prefix
+    myFunc[3] = 0xcc; // breakpoint
+    ((FUN)myFunc)();
 }
